60fb45393b1df770b8e59b51e966cb9ecadcab98
Lariel <lariel.negreiros@gmail.com>
2021-04-28 21:35:43 -0300

    Add GA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
+import { NavigationEnd, Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
 
+declare var gtag;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(
+    router: Router
+  ) {
+    const navEndEvent$ = router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    );
+    navEndEvent$.subscribe((e: NavigationEnd) => {
+      gtag('config', 'G-0X51F76LV0', {
+        page_path : e.urlAfterRedirects
+      });
+    });
+  }
 
 
 }
