56cd4fa587f7a35f82259797166131a90a3769da
Lariel <lariel.negreiros@pucrs.br>
2021-09-03 23:01:50 -0300

    Add zero a esquerda dos segundos no timer

--- src/app/pipes/MinuteSeconds.pipe.ts
+++ src/app/pipes/MinuteSeconds.pipe.ts
@@ -7,7 +7,11 @@ export class MinuteSecondsPipe implements PipeTransform {
 
   transform(value: number): string {
     const minutes: number = Math.floor(value / 60);
-    return minutes + ':' + (value - minutes * 60);
+    return minutes + ':' + this.left(value - minutes * 60);
+  }
+
+  private left(number): number {
+    return number > 9 ? number : '0' + number;
   }
 
 }
