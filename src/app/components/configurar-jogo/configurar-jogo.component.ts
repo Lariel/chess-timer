19dfb6f9986f32f8a23afd70300609ca2b9feb9b
Lariel <lariel.negreiros@pucrs.br>
2021-09-07 16:24:41 -0300

    Adicionado histórico das partidas recentes

--- src/app/components/configurar-jogo/configurar-jogo.component.ts
+++ src/app/components/configurar-jogo/configurar-jogo.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { Partida } from 'src/app/model/iPartida';
+import { HistoricoService } from 'src/app/services/historico.service';
+import { StateService } from 'src/app/services/state.service';
 
 @Component({
   selector: 'app-configurar-jogo',
@@ -11,13 +14,16 @@ export class ConfigurarJogoComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private titleService: Title
+    private titleService: Title,
+    private historico: HistoricoService
   ) { }
 
   tempoMaximoMinutos;
   tempoMaximoSegundos;
+  historicoPartidas: Partida[];
 
   ngOnInit(): void {
+    this.historicoPartidas = this.historico.buscarHistorico();
   }
 
   confirmarTempo = () => {
@@ -27,8 +33,17 @@ export class ConfigurarJogoComponent implements OnInit {
           timer: this.tempoMaximoSegundos
         }
       });
+      this.historico.atualizarHistorico({
+        tempoMaximoMinutos: this.tempoMaximoMinutos,
+        tempoMaximoSegundos: this.tempoMaximoSegundos
+      });
       this.titleService.setTitle('Jogando - Cronômetro Xadrez');
     }
+  }
+
+  selecionarTempoDoHistorico = (tempoMaximoMinutos) => {
+    this.tempoMaximoMinutos = tempoMaximoMinutos;
+    this.confirmarTempo();
   }
 
   converterMinutosParaSegundos = (t: number): number => {
